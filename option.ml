0d7af6d9f652c30838f4052f32c7760e2c88e0ff
Altor <altor.narthan@gmail.com>
2014-10-18 01:12:36 +0200

    ajout de la fonction bindable, modif de l'implementation de double_bind et correction de bind

--- option.ml
+++ option.ml
@@ -1,18 +1,21 @@
-
 let return x = Some x
 
 let bind m f = match m with
-  | Some x -> Some (f x)
+  | Some x ->  f x
   | _ -> None
 
-let double_bind m1 m2 f =
-  match (m1,m2) with
-  | (None, _) -> None
-  | (_, None) -> None
-  | (Some x1, Some x2) -> Some (f x1 x2)
+
+
 
 let (>>=) = bind
+
+let comp f g x = f (g x)
+
+let bindable f = comp return f
+
+let double_bind m1 m2 f = (m1 >>= f) >>= (fun f  -> m2 >>= (bindable f))
 
 let safe f x = 
   try return (f x) with
     _ -> None
+
--- option.mli
+++ option.mli
@@ -12,11 +12,13 @@ val return : 'a -> 'a option
 
 
 (** [ m >>= f] applique la fonction f a la valeur contenue dans m et renvoi une nouvelle valeur encapsulé dans un environement de type option*)
-val (>>=) : 'a option -> ('a -> 'b) -> 'b option 
+val (>>=) : 'a option -> ('a -> 'b option) -> 'b option 
 
 (** double_bind à le même rôle que >>= mais applique une fonction à 2 valeur contenue dans un environement de type option pour renvoyé une nouvelle valeur de type option*)
-val double_bind : 'a option -> 'a option -> ('a -> 'a -> 'b) -> 'b option
+val double_bind : 'a option -> 'b option -> ('a -> ('b -> 'c) option) -> 'c option
 
+(** [bindable f] transforme une fonction de type ['a -> 'b] en fonction de type ['a -> 'b option ] *)
+val bindable : ('a -> 'b) -> 'a -> 'b option
 (** [safe f x] applique une f, une fonction pouvant potentiellement renvoyé une exeption, à x.
 La valeur de retour de f est ensuite encapsulé dans un environement de type option*)
 val safe : ('a -> 'b) -> 'a -> 'b option
