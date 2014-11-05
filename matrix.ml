e691c8c82a893860e00771e9ca4d829de4f218dd
altor <arthur.dazemar@free.fr>
2014-11-05 12:19:29 +0100

    correction bug de type pour Matrix.init

--- matrix.ml
+++ matrix.ml
@@ -1,5 +1,5 @@
 let init l h f =
-  let m = Array.make_matrix l h 0
+  let m = Array.make_matrix l h (f 0 0)
   in let rec aux i =
        if i = l then m
        else let rec aux2 j =
@@ -7,5 +7,3 @@ let init l h f =
 	      else (m.(i).(j) <- (f i j); aux2 (j+1))
 	    in (aux2 0; aux (i+1))
      in aux 0
-
-	      
