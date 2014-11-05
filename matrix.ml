9b0267d75868e024a4fdddc15beb65fced449621
altor <arthur.dazemar@free.fr>
2014-11-05 13:48:24 +0100

    ajout des commande init et map

--- matrix.ml
+++ matrix.ml
@@ -1,9 +1,25 @@
-let init l h f =
-  let m = Array.make_matrix l h (f 0 0)
+let init h l  f =
+  let m = Array.make_matrix h l (f 0 0)
   in let rec aux i =
-       if i = l then m
+       if i = h then m
        else let rec aux2 j =
-	      if j = h then ()
+	      if j = l then ()
 	      else (m.(i).(j) <- (f i j); aux2 (j+1))
 	    in (aux2 0; aux (i+1))
      in aux 0
+
+let length m =
+  (Array.length m, Array.length m.(0))
+
+
+let map f m =
+  let (h,l) = length m
+  in let m2 = Array.make_matrix h l (f m.(0).(0))
+     in for i = 0 to h - 1do
+	 for j = 0 to l - 1do
+	   m2.(i).(j) <- f m.(i).(j)
+	 done
+       done;
+     m2
+      
+  
--- matrix.mli
+++ matrix.mli
@@ -1,2 +1,9 @@
-(** [init l h f] renvoie une matrice de taille l par h ou chaque élément de coordonée (i,j) est initialiser avec la valeur [f i j]*)
+(** [init h l f] renvoie une matrice de taille l par h ou chaque élément de coordonée (i,j) est initialiser avec la valeur [f i j]*)
 val init : int -> int -> (int ->  int-> 'a) -> 'a array array
+
+(** [map f m] remplis chaque case de la matrice m avec le résultat de la fonction f appliqué au contenue de la case*)
+val map : ('a -> 'b) -> 'a array array -> 'b array array
+
+(** renvoi un couple d'entier contenant la longueur et la hauteur de la matrice*)
+val length : 'a array array -> int * int
+
