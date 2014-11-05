499a97b6ca12cb1f8deef95e6eb935826954f941
altor <arthur.dazemar@free.fr>
2014-11-05 14:33:02 +0100

    ajout des commande shuffle et l_shift dans le module Array_mp

--- array_mp.ml
+++ array_mp.ml
@@ -4,3 +4,35 @@ let index_of tab elem =
     | i when tab.(i) = elem -> Some i
     | i -> aux (i+1)
   in aux 0
+
+let l_shift tab index n =
+  let taille = Array.length tab in
+  if index >= taille then failwith "l_shift : erreur indice de départ trop grand"
+  else if n > index then failwith " l_shift : décalage trop grand"
+  else for i = index to taille - 1 do
+      tab.(i-n) <- tab.(i)
+    done
+      
+
+let shuffle tab = 
+  Random.self_init ();
+  let t = Array.length tab in
+  let tab2 = Array.make t (tab.(0))
+  in let rec aux taille = 
+       if taille = 1 then begin
+	 tab2.(0) <- tab.(0);
+	 tab2
+       end
+       else begin 
+	 let i = Random.int taille
+	 in Printf.printf "%d %d\n" taille i;
+	 tab2.(taille-1) <- tab.(i);
+	 if i != taille - 1 then
+	   l_shift tab (i+1) 1
+	 else ();
+	 aux (taille - 1)
+       end
+     in aux t
+	     
+	   
+    
--- array_mp.mli
+++ array_mp.mli
@@ -1,1 +1,11 @@
+(** [index_of tab elem] renvoi un type option contenant l'index de la premiére ocurence de elem dans tab ou None si tab ne contient pas elem*)
 val index_of : 'a array -> 'a -> int option
+
+(** mélange les éléments du tableau passé en argument*)
+val shuffle : 'a array -> 'a array
+
+(** [l_shift tab index n ] décale les élément a gauche d'index de n rangs vers la droite . Les anciennes valeurs écrasé par le décalage.
+Lance une exeption si index dépasse la taille du tableau ou si le décalage est superieur à l'indice de départ *)
+val l_shift : 'a array -> int -> int -> unit
+  
+
