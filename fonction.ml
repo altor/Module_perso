6db8c9eeb5c4ba87ddd6d9e837d7559a5fd728aa
Altor <altor.narthan@gmail.com>
2014-12-18 00:16:37 +0100

    ajout de l'opérateur d'inversion d'argument au module Fonction

--- fonction.ml
+++ fonction.ml
@@ -1,7 +1,13 @@
 let comp f g x = f (g x)
 
+let inv f x y = f y x
+
+let (--) f = inv f		  
+		   
 let (|-) f g = comp f g
 
+
+		    
 let ($) f g = f g
 
 let id x = x
--- fonction.mli
+++ fonction.mli
@@ -4,11 +4,18 @@ val (|-) : ('a -> 'b) -> ('c -> 'a) -> 'c -> 'b
 (** opérateur d'application a moindre priorité.
     [f $ g a] est équivalent à [f (g a)]
     c'est juste pour ne pas utiliser les paranthése
-*)
+ *)
+
+
 val ($) : ('a -> 'b) -> 'a -> 'b
 
+(** [(--) f] inverse l'ordre des arguments de f
+ainsi [((--) f) y x = f x y]*)
+val (--) : ('a -> 'b -> 'c) -> ('b -> 'a -> 'c)
+				
 (** fonction identité *)
 val id : 'a -> 'a
 
 (** fonction couple *)
 val couple : 'a -> 'b -> 'a * 'b
+				
