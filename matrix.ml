57328b8a32cc422bd603d08394201b34b6aa1384
Altor <altor.narthan@gmail.com>
2015-02-02 22:43:29 +0100

    ajout des fonctions iter et iteri au module Matrix

--- matrix.ml
+++ matrix.ml
@@ -15,11 +15,18 @@ let length m =
 let map f m =
   let (h,l) = length m
   in let m2 = Array.make_matrix h l (f m.(0).(0))
-     in for i = 0 to h - 1do
-	 for j = 0 to l - 1do
-	   m2.(i).(j) <- f m.(i).(j)
-	 done
-       done;
-     m2
+     in for i = 0 to h - 1 do
+	  for j = 0 to l - 1 do
+	    m2.(i).(j) <- f m.(i).(j)
+	  done
+	done;
+	m2
+
+let iter f m =
+  Array.iter (Array.iter f) m
+
+let iteri f m =
+  let f2 i = Array.iteri (f i)
+  in Array.iteri f2 m
       
   
--- matrix.mli
+++ matrix.mli
@@ -4,6 +4,11 @@ val init : int -> int -> (int ->  int-> 'a) -> 'a array array
 (** [map f m] remplis chaque case de la matrice m avec le résultat de la fonction f appliqué au contenue de la case*)
 val map : ('a -> 'b) -> 'a array array -> 'b array array
 
-(** renvoi un couple d'entier contenant la longueur et la hauteur de la matrice*)
+(** renvoi un couple d'entier contenant la hauteur et la longueur de la matrice*)
 val length : 'a array array -> int * int
 
+(** [iter f m] applique la fonction f a tout les élément de m **)
+val iter : ('a -> unit) -> 'a array array -> unit
+
+(** [iteri f m] comme iter mais la fonction f prend en parametre la position de l'élément par rapport à l'axe des ordonées et à l'axe des abssice**)
+val iteri : (int -> int -> 'a -> unit) -> 'a array array -> unit
