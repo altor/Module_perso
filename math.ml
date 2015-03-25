a1c69b66b851b3933739be4eddd2276b7a1e50eb
Altor <altor.narthan@gmail.com>
2015-03-25 23:39:48 +0100

    modification de la fonction Math.pow_int pour la rendre plus performante

--- math.ml
+++ math.ml
@@ -4,11 +4,10 @@ let length_int n =
     else aux (acc + 1) (n / 10)
   in aux 0 n
 
-let pow_int a n =
-  let rec aux acc n =
-      if n = 0 then acc
-      else aux (acc * a) (n - 1)
-  in aux 1 n  
+let rec pow_int a n =
+  if n = 0 then 1
+  else let r = pow_int a (n / 2)
+       in r * r * (if n mod 2 = 0 then 1 else a)
 
 let eucl_div n1 n2 =
   ( n1 / n2, n1 mod n2)
